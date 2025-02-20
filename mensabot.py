f0e12f8457067f6d67b56370f748e1ac8a81a21c
Kazuki Adachi <kadachi@student.42tokyo.jp>
2025-02-20 13:17:03 +0900

    asd

--- mensabot.py
+++ mensabot.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 
-# 1時間毎に日本メンサ試験ページ(https://mensa.jp/exam/)をスクレイピングして、
-# 新着情報があればメールに送信するスクリプト
-
 import requests
-from bs4 import BeautifulSoup
 import smtplib
 from email.mime.text import MIMEText
 from email.utils import formatdate
-import time	# スリープ用
-import datetime
+import time
 
 # メール送信用の設定
 URL = 'https://mensa.jp/exam/'
@@ -30,8 +25,6 @@ def create_message(from_addr, to_addr, subject, body):
 
 # メール送信関数
 def send(from_addr, to_addrs, msg):
-
-	# GmailのSMTPサーバを使う場合
 	smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
 	smtpobj.ehlo()
 	smtpobj.starttls()
@@ -41,29 +34,22 @@ def send(from_addr, to_addrs, msg):
 
 # メイン処理
 def main():
-	# 1時間毎に実行
+	oldres = requests.get(URL)
 	while True:
-		# 日本メンサ試験ページをスクレイピング
-		response = requests.get(URL)
-		response.encoding = response.apparent_encoding
-		soup = BeautifulSoup(response.text, 'html.parser')
-		news = soup.find_all('div', class_='news')
-		if len(news) > 0:
-			# 更新情報があればメール送信
-			print('Updated: ' + URL)
+		newres = requests.get(URL)
+		if newres.status_code != 200:
+			print('Connection Error')
+			time.sleep(10)
+			continue
+		if newres.text != oldres.text:
+			print('Updated')
 			msg = create_message(FROM_ADDRESS, TO_ADDRESS, SUBJECT, BODY)
 			send(FROM_ADDRESS, [TO_ADDRESS], msg)
 		else:
 			print('No updates')
-		# 1時間スリープ
+		oldres = newres
 		time.sleep(3600)
 
 if __name__ == '__main__':
 	main()
-
-# このスクリプトを実行すると、1時間毎に日本メンサ試験ページをスクレイピングして、
-# 更新情報があればメールに送信します。
-# メール送信にはGmailのSMTPサーバを使用しています。
-# 送信元メールアドレスと送信先メールアドレス、パスワードを設定してください。
-# メール送信に成功すると、コンソールに"No updates"と表示されます。
-# 更新情報があれば、日本メンサ試験ページのURLが表示されます。
+	
