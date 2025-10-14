9c4b6d436b61ab1150ea5937c41c9c092fe90f7e
liqsuq <liqsuq@rkgk.net>
2025-10-14 23:34:08 +0900

    メール送信機能を削除し、通知をntfy.shを使用して送信するように変更

--- mensabot.py
+++ mensabot.py
@@ -1,54 +1,42 @@
 #!/usr/bin/python3
 
 import requests
-import smtplib
-from email.mime.text import MIMEText
-from email.utils import formatdate
 import time
 
-# メール送信用の設定
-URL = 'https://mensa.jp/exam/'
-FROM_ADDRESS = '送信元メールアドレス'
-MY_PASSWORD = '送信元メールアドレスのパスワード'
-TO_ADDRESS = '送信先メールアドレス'
-SUBJECT = '日本メンサ試験ページ更新情報'
-BODY = '日本メンサ試験ページに更新がありました。\n' + URL
+STRF = "[%y/%m/%d %H:%M]"
+TARGET_URL = "https://mensa.jp/exam/"
+NOTIFY_URL = "https://ntfy.sh/mensabot"
+HEAD = "MensaBot"
+BODY = "Mensa試験情報が更新されました。".encode('utf-8')
+TAGS = "tada"
 
-# メッセージ作成関数
-def create_message(from_addr, to_addr, subject, body):
-	msg = MIMEText(body)
-	msg['Subject'] = subject
-	msg['From'] = from_addr
-	msg['To'] = to_addr
-	msg['Date'] = formatdate()
-	return msg
+def send_msg():
+	r = requests.post(
+		NOTIFY_URL,
+		headers = {"Title": HEAD, "Click": TARGET_URL, "Tags": TAGS},
+		data = BODY,
+	)
+	if r.status_code != 200:
+		print(time.strftime(STRF), "Failed to send: ", r.reason)
 
-# メール送信関数
-def send(from_addr, to_addrs, msg):
-	smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
-	smtpobj.ehlo()
-	smtpobj.starttls()
-	smtpobj.login(FROM_ADDRESS, MY_PASSWORD)
-	smtpobj.sendmail(from_addr, to_addrs, msg.as_string())
-	smtpobj.close()
-
-# メイン処理
 def main():
-	oldres = requests.get(URL)
+	print(time.strftime(STRF), "MensaBot started")
+	while (r := requests.get(TARGET_URL)).status_code != 200:
+		print(time.strftime(STRF), "Failed to fetch: ", r.reason)
+		time.sleep(5)
+	old = '\n'.join(l.strip() for l in r.text.splitlines() if l.strip())
 	while True:
-		newres = requests.get(URL)
-		if newres.status_code != 200:
-			print('Connection Error')
-			time.sleep(10)
-			continue
-		if newres.text != oldres.text:
-			print('Updated')
-			msg = create_message(FROM_ADDRESS, TO_ADDRESS, SUBJECT, BODY)
-			send(FROM_ADDRESS, [TO_ADDRESS], msg)
+		while (r := requests.get(TARGET_URL)).status_code != 200:
+			print(time.strftime(STRF), "Failed to fetch: ", r.reason)
+			time.sleep(5)
+		new = '\n'.join(l.strip() for l in r.text.splitlines() if l.strip())
+		if new != old:
+			print(time.strftime(STRF), 'Updated')
+			send_msg()
 		else:
-			print('No updates')
-		oldres = newres
-		time.sleep(3600)
+			print(time.strftime(STRF), 'No updates')
+		old = new
+		time.sleep(300)
 
 if __name__ == '__main__':
 	main()
